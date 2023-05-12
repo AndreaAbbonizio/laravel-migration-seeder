696e434cfd331439c5b38d07b0a38eb5700c0146
AndreaAbbonizio <andreaabbonizio01@gmail.com>
2023-05-12 16:10:22 +0200

    exercise done.

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -16,5 +16,20 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        for ($i = 0; $i < 10; $i++) {
+            $newTrain = new Train();
+
+            $newTrain->azienda = $faker->words(2, true);
+            $newTrain->stazione_di_partenza = $faker->city();
+            $newTrain->stazione_di_arrivo = $faker->city();
+            $newTrain->orario_di_partenza = $faker->dateTimeBetween('-10 days', '+2 month')->format('Y-m-d H:i:s');
+            $newTrain->orario_di_arrivo = $faker->dateTimeBetween('-10 days', '+2 month')->format('Y-m-d H:i:s');
+            $newTrain->codice_treno = $faker->regexify('[A-Z]{2}[0-4]{2}');
+            $newTrain->in_orario = $faker->boolean();
+            $newTrain->cancellato = $faker->boolean();
+
+
+            $newTrain->save();
+        }
     }
 }
