925fe6a7908af07f84e71d94e742697ed1a3aed1
AndreaAbbonizio <andreaabbonizio01@gmail.com>
2023-05-12 16:58:27 +0200

    bonus done.

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -10,7 +10,7 @@ class PageController extends Controller
 {
     public function home()
     {
-        $trains =  Train::where('orario_di_partenza', '>=', now()->format('Y-m-d'))->get();;
+        $trains =  Train::all();
         return view('home', compact('trains'));
     }
 }
--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -16,20 +16,50 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i = 0; $i < 10; $i++) {
+
+
+        $trainFile = fopen(__DIR__ . '/../trains.csv', 'r');
+
+        $trainCSV = fgetcsv($trainFile);
+
+        $trainCSV = fgetcsv($trainFile);
+
+        while ($trainCSV != false) {
             $newTrain = new Train();
 
-            $newTrain->azienda = $faker->words(2, true);
-            $newTrain->stazione_di_partenza = $faker->city();
-            $newTrain->stazione_di_arrivo = $faker->city();
-            $newTrain->orario_di_partenza = $faker->dateTimeBetween('-10 days', '+2 month')->format('Y-m-d H:i:s');
-            $newTrain->orario_di_arrivo = $faker->dateTimeBetween('-10 days', '+2 month')->format('Y-m-d H:i:s');
-            $newTrain->codice_treno = $faker->regexify('[A-Z]{2}[0-4]{2}');
-            $newTrain->in_orario = $faker->boolean();
-            $newTrain->cancellato = $faker->boolean();
+            $newTrain->azienda = $trainCSV[0];
+            $newTrain->stazione_di_partenza = $trainCSV[1];
+            $newTrain->stazione_di_arrivo = $trainCSV[2];
+            $newTrain->orario_di_partenza = $trainCSV[3];
+            $newTrain->orario_di_arrivo = $trainCSV[4];
+            $newTrain->codice_treno = $trainCSV[5];
+            $newTrain->numero_carrozze = $trainCSV[6];
+            $newTrain->in_orario = $trainCSV[7];
+            $newTrain->cancellato = $trainCSV[8];
+
+
 
 
             $newTrain->save();
+
+            $trainCSV = fgetcsv($trainFile);
         }
+
+        // for ($i = 0; $i < 10; $i++) {
+        //     $newTrain = new Train();
+
+
+        //     $newTrain->azienda = $faker->words(2, true);
+        //     $newTrain->stazione_di_partenza = $faker->city();
+        //     $newTrain->stazione_di_arrivo = $faker->city();
+        //     $newTrain->orario_di_partenza = $faker->dateTimeBetween('-10 days', '+2 month')->format('Y-m-d H:i:s');
+        //     $newTrain->orario_di_arrivo = $faker->dateTimeBetween('-10 days', '+2 month')->format('Y-m-d H:i:s');
+        //     $newTrain->codice_treno = $faker->regexify('[A-Z]{2}[0-4]{2}');
+        //     $newTrain->in_orario = $faker->boolean();
+        //     $newTrain->cancellato = $faker->boolean();
+
+
+        //     $newTrain->save();
+        // }
     }
 }
